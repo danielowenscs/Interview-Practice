5ee79ec68d68bd42570da26525380ab0c8c48714
Daniel Owens <115508065+danielowenscs@users.noreply.github.com>
2023-03-08 20:40:28 -0500

    Time: 17 ms (72.34%), Space: 13.3 MB (99.89%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = ''
+        for char in s:
+            if (char == '('):
+                stack += ')'
+            elif (char == '['):
+                stack += ']'
+            elif (char == '{'):
+                stack += '}'
+            elif (len(stack) == 0 or char != stack[-1]):
+                return False
+            else:
+                stack = stack[:-1]
+
+        return (len(stack) == 0)
+    
+    
+
+        
